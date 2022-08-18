0ce157699e45bfe9f0bb7e9c03d24714a6bfc557
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2022-08-18 16:47:24 +0300

    AOP

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -21,21 +22,13 @@ namespace Business.Concrete
             _rentalDal = rentalDal;
 
         }
+        [ValidationAspect(typeof(RentalValidator))]
         public IResult Add(Rental rental)
         {
-            ValidationTool.Validate(new RentalValidator(), rental);
+            // ValidationTool.Validate(new RentalValidator(), rental);
 
-            if (rental.ReturnDate != null)
-            {
-                _rentalDal.Add(rental);
-                return new SuccessResult(Messages.Added);
-            }
-            else
-            {
-                return new ErrorResult(Messages.NotAdded);
-            }
-
-
+            _rentalDal.Add(rental);
+            return new SuccessResult(Messages.Added);
         }
 
         public IResult Delete(Rental rental)
